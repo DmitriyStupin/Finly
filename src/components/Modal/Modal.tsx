6a8ac98a40bfbca5a01ea924e05bcaa7bbf5f38d
Dmitry Stupin <dm.stup@mail.ru>
2026-01-03 12:02:48 +0700

    created a Modal component that serves as a wrapper for the children that will be passed to it.

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,8 +1,28 @@
-const Modal = () => {
+import styles from './Modal.module.scss';
+import type { ReactNode } from 'react';
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+};
+
+const Modal = (props: Props) => {
+  const { isOpen = false, onClose, children } = props;
+
+  if (!isOpen) return null;
+
   return (
-    <div>
-      <div>a;k;alkd;lad</div>
-      <div>a;k;alkd;lad</div>
+    <div className={styles.modalOverlay} onClick={onClose}>
+      <div
+        className={styles.modalContent}
+        onClick={(event) => event.stopPropagation()}
+      >
+        <button className={styles.modalClose} type="button" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
     </div>
   );
 };
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,13 +2,33 @@ import TransactionsCardsSection from '../../sections/TransactionsCardsSection';
 import clsx from 'clsx';
 import styles from './HomePage.module.scss';
 import TransactionsListSection from '../../sections/TransactionsListSection';
+import Modal from '../../components/Modal';
+import { useState } from 'react';
 
 const HomePage = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const openModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
     <div className={clsx(styles.homePageInner, 'container')}>
       <h1>Главная страница</h1>
       <TransactionsCardsSection />
       <TransactionsListSection />
+      <button onClick={openModal} type="button">
+        Открыть модальное окно
+      </button>
+      <Modal isOpen={isOpenModal} onClose={closeModal}>
+        ajkk;aldsask;djaklssss ajkk;aldsask;djaklssss
+        ajkk;aldsask;djaklssssajkk;aldsask;djaklssss ajkk;aldsask;djaklssss
+        ajkk;aldsask;djaklssss ajkk;aldsask;djaklssss ajkk;aldsask;djaklssss
+      </Modal>
     </div>
   );
 };
